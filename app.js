dba54ff329a9d806928a99865cbca42e57c23671
jjcasco99 <juanjocasco28@gmail.com>
2022-01-15 09:58:20 +0100

    path installed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // MODULES
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
-
+const path = require("path")
 const app = express();
 
 // MIDDLEWARES
@@ -10,6 +10,11 @@ app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.use('/api/users', userRoutes);
+
+// HEROKU STATIC FILES
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'client/public/index.html'));
+  });
 
 // Export app
 module.exports = app;
