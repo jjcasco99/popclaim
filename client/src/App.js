8450aed5c34da044c3a3e62c11e6f13507cb6a9d
jjcasco99 <juanjocasco28@gmail.com>
2022-01-18 23:24:59 +0100

    log route first try

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 // import Header from './components/Header';
 import Main from './components/Main';
+import Login from './components/Login';
+import { useAuth0 } from '@auth0/auth0-react';
 
 import {BrowserRouter} from 'react-router-dom';
 
 function App() {
+  const { isAunthenticated } = useAuth0();
+
   return (
     <div className="App">
       <BrowserRouter>
       <div>
+        {isAunthenticated ? (
+          <>
+            <Main />
+          </>
+          ):(
+            <Login/>
+          )}
         <Main />
       </div>
       </BrowserRouter>
--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -6,37 +6,47 @@ import Keywords from './Keywords';
 import Cardlist from './Cardlist';
 import Header from './Header'
 
+import { useAuth0 } from '@auth0/auth0-react';
+
 const Dashboard = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="dashboard">
-      <Header />
-      <div>
-      <section className="cabecera-dashboard">
-        <div className="cabecera-flex">
-          <h1 className="cabecera-titulo">REPUTACIÓN BASH</h1>
-          <h2 className="cabecera-año">
-            Año/s 2022 <img src={arrow} alt="" />
-          </h2>
-          <h2 className="cabecera.mes">
-            Mes/s Enero <img src={arrow} alt="" />
-          </h2>
+    isAuthenticated && (
+      <div className="dashboard">
+        <Header />
+        <div>
+          <section className="cabecera-dashboard">
+            <div className="cabecera-flex">
+              <h1 className="cabecera-titulo">REPUTACIÓN BASH</h1>
+              <h2 className="cabecera-año">
+                Año/s 2022 <img src={arrow} alt="" />
+              </h2>
+              <h2 className="cabecera.mes">
+                Mes/s Enero <img src={arrow} alt="" />
+              </h2>
+            </div>
+            <div className="cabecera-flex">
+              <input type="submit" value="Actualizar datos" id="data" />
+              <input type="submit" value="Generar informe" id="create-inform" />
+            </div>
+          </section>
+          <div className="flex-container">
+            <section>
+              <Reputation />
+              <Keywords />
+            </section>
+            <section>
+              <Cardlist />
+            </section>
+          </div>
         </div>
-        <div className="cabecera-flex">
-          <input type="submit" value="Actualizar datos" id="data" />
-          <input type="submit" value="Generar informe" id="create-inform" />
-        </div>
-      </section>
-      <div className="flex-container">
-        <section>
-          <Reputation />
-          <Keywords />
-        </section>
-        <section>
-          <Cardlist />
-        </section>
       </div>
-      </div>
-    </div>
+    )
   );
 };
 
