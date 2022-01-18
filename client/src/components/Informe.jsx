48398ad5787ea23e4f50cd606606e5b69ee1ed00
Ricardo <bescir@gmail.com>
2022-01-18 20:23:33 +0100

    Report component implemented

--- client/src/components/Informe.jsx
+++ client/src/components/Informe.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import './Informe.css'
-import arrow from '../assets/caret--right.png'
+import React from 'react';
+import Report from './Report';
+import Header from './Header';
+import './Informe.css';
+import arrow from '../assets/caret--right.png';
 
 const Informe = () => {
-    return (
-        <div className='global'>
-            <div className='cabecera-informe'>
-                <h1>INFORME BASH</h1>
-                <h2>Año/s 2022 <img src={arrow} alt="" /></h2>
-                <h2>Mes/s Enero <img src={arrow}  alt="" /></h2>
-                <input type="submit" value="Descargar" id='download' />
-            </div>
+  return (
+    <section className="global">
+      <Header />
+      <div>
+        <div className="cabecera-informe">
+          <h1>INFORME BASH</h1>
+          <h2>
+            Año/s 2022 <img src={arrow} alt="" />
+          </h2>
+          <h2>
+            Mes/s Enero <img src={arrow} alt="" />
+          </h2>
+          <input type="submit" value="Descargar" id="download" />
         </div>
-        
-    )
-}
+        <div>
+          <Report />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Informe
+export default Informe;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
-import Informe from './Informe'
-import {Route, Routes} from 'react-router-dom';
+import Informe from './Informe';
+import { Route, Routes } from 'react-router-dom';
 
 const Main = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/dashboard" element={<Dashboard />}/>
-        <Route path="/informe" element={<Informe />}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/informe" element={<Informe />} />
       </Routes>
     </div>
   );
