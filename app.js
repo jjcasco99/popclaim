6e4c32aa9b1dbffff19a7dd193f25df1ff5a9291
Ricardo <bescir@gmail.com>
2022-01-19 13:43:14 +0100

    nothing changed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // MODULES
 const express = require('express');
+const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const tweetRoutes = require('./routes/tweetRoutes');
 const path = require('path');
+
 const app = express();
 
 // MIDDLEWARES
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
